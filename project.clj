77a9b79a2a0a6218155834d7ef9b15732c9dd823
Andrew Stine <stine.drew@gmail.com>
2014-10-14 16:57:25 -0400

    updated project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject codox-info "0.1.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Info file API generator for Clojure"
+  :url "https://github.com/astine/codox-info"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
